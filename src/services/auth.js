07503f58760106c7fae4dfee96e22cced45d60ce
ivandax <ivandax_89@hotmail.com>
2020-03-15 21:49:36 +0100

    started checking 2 step auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,31 @@
 import firebase from 'firebase/app';
 import "firebase/auth";
 
+// var actionCodeSettings = {
+//     url: 'https://cherry-rate-app.firebaseapp.com/',
+//     handleCodeInApp: true,
+//     iOS: {
+//       bundleId: 'com.example.ios'
+//     },
+//     android: {
+//       packageName: 'com.example.android',
+//       installApp: true,
+//       minimumVersion: '12'
+//     },
+//     dynamicLinkDomain: 'example.page.link'
+// };
+
+// async function signUpLink(email,actionCodeSettings){
+//     const result = firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+//     .then(function() {
+//         console.log("email sent!!!")
+//         window.localStorage.setItem('emailForSignIn', email);
+//     })
+//     .catch(function(error) {
+//         console.log(error)
+//     });
+// }
+
 function signup(email, password){
     firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error){
         console.log(error);
@@ -17,6 +42,7 @@ function registerAuthObserver(callback) {
 
 async function login(email,password){
     const result = await firebase.auth().signInWithEmailAndPassword(email,password);
+    console.log(result)
     return result.user.uid; //returns true if successful
 }
 
